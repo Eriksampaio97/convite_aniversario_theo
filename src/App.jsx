00a204cb3faa57ae1c70843365ed93240920a977
Erik Sampaio <erik.sampaio@bideafactory.com>
2024-02-13 17:07:15 -0300

    inicio do convite

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* <div>Hello World!</div> */}
+      <div className='card'>
+        <div className='container__info'>
+          <div className="content_info">
+            <p className='title'>Estou completando mais uma fase!</p>
+            <p className='subtitle'>Venha comemorar comigo!</p>
+          </div>
+        </div>
+        <div className='container__name'>
+          <div className="content_name">
+            <p className='nameText'>Theo</p>
+            <p className='age'>9 Anos</p>
+          </div>
+        </div>
+        <div className='container__date'>
+          <div className="content_date">
+            <p className='day'>Quarta</p>
+            <p className='date'>20/03</p>
+          </div>
+          <div className="content_locale">
+            <p className='hour'>Às 20:30hrs</p>
+            <p className='date'>local</p>
+          </div>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
