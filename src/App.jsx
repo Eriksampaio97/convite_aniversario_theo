5fd3a345629a19301e8f6bf84f8d0b146a443af7
Erik Sampaio <erik.sampaio@bideafactory.com>
2024-02-17 17:18:40 -0300

    atualizacoes no convite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import localizacao from './assets/localizacao.png'
+import confirmation from './assets/confirmacao.png'
+import gift from './assets/presente.png'
 import './App.css'
 
 function App() {
@@ -23,11 +25,25 @@ function App() {
         <div className='container__date'>
           <div className="content_date">
             <p className='day'>Quarta</p>
-            <p className='date'>20/03</p>
+            <p className='date'>20</p>
+            <p className='month'>Março</p>
           </div>
           <div className="content_locale">
             <p className='hour'>Às 20:30hrs</p>
-            <p className='date'>local</p>
+            <div className='buttons'>
+              <div className='container__button'>
+                <a href='www.google.com' className='optionButton'><img src={localizacao} alt="" className='button_image'/></a>
+                <p className='buttonText'>localização</p>
+              </div>
+              <div className='container__button'>
+              <a href='www.google.com' className='optionButton'><img src={confirmation} alt="" className='button_image'/></a>
+                <p className='buttonText'>Confirmação</p>
+              </div>
+              <div className='container__button'>
+                <button className='optionButton'><img src={gift} alt="" className='button_image'/></button>
+                <p className='buttonText'>Dicas de presente</p>
+              </div>
+            </div>
           </div>
         </div>
 
